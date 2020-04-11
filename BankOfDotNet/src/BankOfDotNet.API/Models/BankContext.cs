cf17db2ca02f79e86718fac1ec56d7c47e1efa0f
renan santos <renanwillian87@gmail.com>
2020-04-10 22:57:26 -0300

    Projetos IdentitySrv, ConsoleClient, WebAPIClient Adicionados

---
+++ BankOfDotNet/src/BankOfDotNet.API/Models/BankContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfDotNet.API.Models
+{
+    public class BankContext : DbContext
+    {
+        public BankContext(DbContextOptions<BankContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
---
+++ BankOfDotNet/src/BankOfDotNet.ConsoleClient/Program.cs
@@ -0,0 +1,67 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOfDotNet.ConsoleClient
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            //discover all the endpoints using metada of identity server
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if(disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // Grab a bearer token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("BankOfDotNetApi");
+
+            if(tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            //Consume our customer API 
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var customerInfo = new StringContent(
+                JsonConvert.SerializeObject(
+                        new { Id = 1, FirstName = "Renan", LastName = "Santos" }
+                    ), Encoding.UTF8, "application/json");
+
+            var createCustomerResponse = await client.PostAsync("http://localhost:60169/api/Customers", customerInfo);
+
+            if (!createCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(createCustomerResponse.StatusCode);
+            }
+
+            var getCustomerResponse = await client.GetAsync("http://localhost:60169/api/Customers");
+            if(!getCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(getCustomerResponse.StatusCode);
+            }
+            else
+            {
+                var content = await getCustomerResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+            Console.Read();
+        }
+    }
+}
